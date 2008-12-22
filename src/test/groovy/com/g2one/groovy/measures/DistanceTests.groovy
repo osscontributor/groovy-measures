039acd1fdb6089abac4dfe23110ddc91bd2d5295
Jeff Brown <jeff@jeffandbetsy.net>
2008-12-22 17:42:02 -0600

    more tests

--- src/test/groovy/com/g2one/groovy/measures/DistanceTests.groovy
+++ src/test/groovy/com/g2one/groovy/measures/DistanceTests.groovy
@@ -8,32 +8,40 @@ class DistanceTests extends GroovyTestCase {
 
     void testRetrievingMetricValues() {
         def len = 2.kilometers
-        assertEquals 2.0,     len.kilometers
-        assertEquals 2.0E3,   len.meters
-        assertEquals 2.0E5,   len.centimeters
-        assertEquals 2.0E6,   len.millimeters
-        assertEquals 2.0E9,   len.micrometers
-        assertEquals 2.0E12,  len.nanometers
+        assertEquals 2.0,    len.kilometers
+        assertEquals 2.0E3,  len.meters
+        assertEquals 2.0E5,  len.centimeters
+        assertEquals 2.0E6,  len.millimeters
+        assertEquals 2.0E9,  len.micrometers
+        assertEquals 2.0E12, len.nanometers
 
         def len2 = 400.centimeters
-        assertEquals 4.0E-3,   len2.kilometers
-        assertEquals 4.0,      len2.meters
-        assertEquals 4.0E2,    len2.centimeters
-        assertEquals 4.0E3,    len2.millimeters
-        assertEquals 4.0E6,    len2.micrometers
-        assertEquals 4.0E9,    len2.nanometers
+        assertEquals 4.0E-3, len2.kilometers
+        assertEquals 4.0,    len2.meters
+        assertEquals 4.0E2,  len2.centimeters
+        assertEquals 4.0E3,  len2.millimeters
+        assertEquals 4.0E6,  len2.micrometers
+        assertEquals 4.0E9,  len2.nanometers
+    }
+
+    void testMetricConversions() {
+        assertEquals 2.0.kilometers,    2.0E3.meters
+        assertEquals 2.0E3.meters,      2.0E5.centimeters
+        assertEquals 2.0E5.centimeters, 2.0E6.millimeters
+        assertEquals 2.0E6.millimeters, 2.0E9.micrometers
+        assertEquals 2.0E9.micrometers, 2.0E12.nanometers
     }
 
     void testRetrievingImperialValues() {
         def len = 5.miles
-        assertEquals 5, len.miles
-        assertEquals 8800, len.yards
-        assertEquals 26400, len.feet
+        assertEquals 5,      len.miles
+        assertEquals 8800,   len.yards
+        assertEquals 26400,  len.feet
         assertEquals 316800, len.inches
     }
 
     void testImperialConversions() {
-        assertEquals 5.miles, 8800.yards
+        assertEquals 5.miles,    8800.yards
         assertEquals 8800.yards, 26400.feet
         assertEquals 26400.feet, 316800.inches
     }
